e132df595a1acdf589b60c4ae1065d1879a22ba8
etan90 <elaine.tan@hotmail.co.uk>
2022-05-07 08:59:17 +0100

    Updated pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,15 @@ let pokemonList = [
     name: 'Charmander',
     height: 2,
     types: ['fire']
-  }  
+  },
+  {
+    name: 'Bulbasaur',
+    height: 2.4,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Charmeleon',
+    height: 3.7,
+    types: ['fire']
+  }
 ];
