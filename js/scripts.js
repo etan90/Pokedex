078e790a87e3745680f62b9865efe6fc909a3f3d
etan90 <elaine.tan@hotmail.co.uk>
2022-05-25 13:10:32 +0100

    Replaced for loop with foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,17 @@ let pokemonList = [
   },
   {
     name: 'Bulbasaur',
-    height: 2.4,
+    height: 2.04,
     types: ['grass', 'poison']
   },
   {
     name: 'Charmeleon',
-    height: 3.7,
+    height: 3.07,
     types: ['fire']
   }
 ];
 
-//prints out Pokemon's name and height
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList.height > 3.0) {
-    document.write(pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + ') --> This is big! | ');
-  } else {
-    document.write(pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + ') | ');
-  }
-}
+//prints out Pokemon's name and height. Height above '3' is considered big.
+pokemonList.forEach((pokemon) => pokemon.height > 3 ?
+document.write('<p>' + pokemon.name + ' ' + '(height: ' + pokemon.height + ') = This is big! </p>'):
+document.write('<p>' + pokemon.name + ' ' + '(height: ' + pokemon.height + ') </p>'))
